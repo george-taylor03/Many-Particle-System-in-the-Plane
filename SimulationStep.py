1f378d64dd01f92cdca33ed8cd20352f3c928d82
George Taylor <121175619+george-taylor03@users.noreply.github.com>
2026-03-06 10:48:54 +0000

    New simulationstep using numba
    Used new numba script to make a dedicated independent script that can be run seperatly. Fixed some issues with globals so will run.

--- SimulationStep.py
+++ SimulationStep.py
@@ -1,156 +1,180 @@
-#Imports 
-
 import numpy as np
-from math import *
+import matplotlib.pyplot as plt
+import math
+import time
+from matplotlib.patches import Rectangle
+from numba import njit, prange, int64 #--Numba Ver--#
+from numba.typed import List, Dict #--Numba Ver--#
+from numba.core import types #--Numba Ver--#
 
+#~---------------------------------------------------------------------------------------------------------------------------------------------------~#
 
-def gridBuilder(x,box,N):
-    pass
-    #Assign gridLength
-    gridLength = 0.4
+# Numba Notes:
+    # User manual - https://numba.readthedocs.io/en/stable/user/index.html
+    # Lines with comments '#--Numba Ver--#' -> uncommenting = Numba version
+    # Lines with comments '#--Python Ver--#' -> uncommenting = Python version
+        # Choose one version to run
+    # If creating code in Python first -> avoid dictionaries where possible (initializing a numba.typed.dict (like in simulation_step) can be slow)
 
-    #Get number of grid rows and columns
-    rows = int(box[1][0] / gridLength)
-    cols = int(box[1][0] / gridLength)
+# Potential Improvements:
+    # create_grid_index and create_grid can be merged -> might make a small change, better readability when separate (in my opinion)
+    # Find way to reduce math.function (e.g. math.cos) in force_particle, ideally get rid of all -> sqrt not as expensive as others
+    # Go through Performance Improvements (Link, User Manual -> Performance Tips) on njit functions, see if time goes down
+        # Already done: fastmath, parallel
 
-    #Zero matrix representing grid positions
-    gridArray = []
+# Small optimisation notes:
+    # x * x faster than x ** 2
+    # math.function faster for numbers, np.function faster for arrays
+    # array[:, n] slower than array[0, n], array[1, n], ...
+    # Splitting x into X (=x[0]) and Y (=x[1]) usually faster
 
-    # build more effective grid assignment build on gridarrys assignments using python dictionaries 
-    grid = {} # empty dict to be appended
+#~---------------------------------------------------------------------------------------------------------------------------------------------------~#
 
-    #Assign Grids
-    for particle in range(N):
-        #Determine Grid Row
+np.random.seed(1)
+ran = np.random.rand
 
-        gridPosRow = int(x[0][particle] // gridLength)
-
-        #Accounting for boundary cases
-        if gridPosRow>(rows-1):
-            gridPosRow -= 1
-
-        gridPosCol = int(x[1][particle] // gridLength)
-
-        #Accounting for boundary cases
-        if gridPosCol>(cols-1):
-            gridPosCol -= 1
-
-        #Append gridPosition to particles index
-        gridArray.append([gridPosRow,gridPosCol])
-
-        # if a box exists just add the particle to the box
-        if (gridPosRow,gridPosCol) in grid:
-            grid[gridPosRow,gridPosCol].append(particle)
-        # if not add a new box 
-        else:
-            grid[gridPosRow,gridPosCol] = [particle]
-    # this sorts much faster as it uses the very efficient dictionaries lookups
-
-    return grid,gridArray,gridLength
-        
-# check each particle against once in its box  or the surrounding 8 boxes 
-def connect(grid, gridArray,N):
-    # create a empty list for each particle to store its nearby particles 
-    # just a list of list [[],[],[],[],[]] <- like this
-    partnet = [[] for j in range(N)]
-    # split our grid array values to into two separate pairs instead of tuples for our dict
-    for i in range(N):
-        xco = gridArray[i][0]
-        yco = gridArray[i][1]
-        # checks the surrounding 3X3 boxes around this particles 
-        # better that nested if loops as for loops are faster 
-        for dxco in (-1,0,1):
-            for dyco in (-1,0,1):
-                # work out the nearby box co-ords e.g if we in box (4,4) we check
-                # (3,3),(3,4),(3,5),(4,4), ....., (5,5)
-                surr = (xco + dxco, yco + dyco)
-                # check if there is a particle in the box no point checking empty boxes
-                if surr in grid:
-                    # add all particles from nearby boxes to out list of nearby particles
-                    partnet[i] += grid[surr]
-    return partnet 
-
-def forceWall(Forces,x,gridLength,box,part,N):
-    #Check collision with wall
-    for particle in range(N):
-        if x[0][particle] <= gridLength or x[0][particle] >= box[1][0] - gridLength or x[1][particle] <= gridLength or x[1][particle] >= box[1][1] - gridLength:
-            #Particle A 
-            A = x[:, particle]
-
-            #Wall Forces
-            fLeft = max(0,part['radius'] + box[0][0] - A[0])
-            fRight = max(0,part['radius'] + A[0] - box[1][0])
-            fBot = max(0,part['radius'] + box[0][1] - A[1])
-            fUp = max(0,part['radius'] + A[1] - box[1][1])
-
-            #Check Wall Collision
-            #X component
-            Forces[0][particle] += part['spring'] * (fLeft - fRight)
-
-            #y component
-            Forces[1][particle] += part['spring'] * (fBot - fUp)
-    return Forces
-
-def forceParticle(Forces,x,partNet,part,N):
-    #Cycle through each particle
-    for particle in range(N):
-        #Cycle through all its connections
-        for neighbour in partNet[particle]:
-                    #Particle A 
-            A = x[:, particle]
-
-            #Other Particle
-            B = x[:,neighbour]
-
-
-            #Distance between them
-            d = np.sqrt((A[0] - B[0])**2 + (A[1] - B[1])**2)
-
-            #Check if will collide
-            if d < 2 * part['radius'] and d>0:
-                #Angle
-                a = np.arctan2(A[1]-B[1],A[0]-B[0])
-            
-                #X component
-                Forces[0][particle] += part['spring'] * (2*part['radius'] - d) * np.cos(a)
-
-                #y component
-                Forces[1][particle] += part['spring'] * (2*part['radius'] - d) * np.sin(a)
-
-    return Forces
-
-def SimulationStep(x:np.ndarray, v:np.ndarray, h:float, part:dict, box:np.ndarray, g:float, N:int) -> tuple[np.ndarray,np.ndarray]:
-    """
-    Preforms one single step forward in time
-
-    Args:
-        h (float): Time step size
-        x (2xN array): Current particle positions (first row are horizontal coordinates, second row are vertical coordinates)
-        v (2xN array): Current particle velocities (first row are horizontal coordinates, second row are vertical coordinates)
-        part (dictionary['radius','spring']): Stores properties of the particle (radius r of all particles and spring constant of all particles)
-        box (2x2 array): Array storing the corners of the box (np.vstack([low,upp]))
-        g (float): Single number, the amount g of gravity
-        N (Integer): Number of particles
-
-    Returns:
-        tuple: xnew(2xN array), vnew(2xN array) which are the updated position and velocity
-    """
-    #If N is not passed into the function:
-    # N = len(x[0])
-
-    #Set up forces array
-    Forces = np.zeros((2,N))
-
-    grid,gridArray,gridLength = gridBuilder(x,box,N)
+# Creates list of all particle's indexes
+@njit #--Numba Ver--#
+def create_grid_index(N, box_length, length, width, X, Y):
+    grid_index = np.empty((2, N), dtype = int64) #--Numba Ver--#
+    #grid_index = np.empty((2, N)) #--Python Ver--#
     
-    partNet = connect(grid,gridArray,N)
+    for particle in range(N):
+        # Position of partcle
+        particle_X, particle_Y = X[particle], Y[particle]
+        
+        # Prevents particle's index from being outside the grid
+        particle_col, particle_row = min(int(particle_X / box_length), length - 1), min(int(particle_Y / box_length), width - 1)
+        
+        grid_index[0, particle] = particle_col
+        grid_index[1, particle] = particle_row
 
-    Forces = forceWall(Forces,x,gridLength,box,part,N)
+    return grid_index
 
-    Forces = forceParticle(Forces,x,partNet,part,N)
+# Returns grid of all particles
+@njit #--Numba Ver--#
+def create_grid(N, length, grid_index, grid):
+    for particle in range(N):
+        # Particle's index
+        particle_col, particle_row = grid_index[0, particle], grid_index[1, particle]
 
-    xnew = x + (h * v) + (h**2)*Forces
-    vnew =(xnew - x)/ h 
+        # Converts (row, col) into integer - https://stackoverflow.com/questions/1730961/convert-a-2d-array-index-into-a-1d-index
+        key = int64(particle_row * length + particle_col) #--Numba Ver--#
+        #key = particle_row * length + particle_col #--Python Ver--#
+        
+        if key not in grid:
+            grid[key] = List.empty_list(int64) # --Numba Ver--
+            #grid[key] = [] #--Python Ver--#
+        
+        grid[key].append(particle)
+        
+    return grid
 
-    return xnew, vnew
+# Checks for collisions with walls
+@njit(fastmath = True, parallel = True) #--Numba Ver--# 
+def force_wall(N, radius, spring, X, Y, box, forces):
+    for particle in prange(N): #--Numba Ver--#
+    #for particle in range(N): #--Python Ver--#
+        # Position of partcle
+        particle_X, particle_Y = X[particle], Y[particle]
 
+        # Only calculate collisions for particles less than radius away from a wall
+        if particle_X < radius or particle_Y < radius or box[1, 0] - radius < particle_X or box[1, 1] - radius < particle_Y:
+            
+            #Assign wall focres
+            f_left = max(0, radius - particle_X)
+            f_right = max(0, radius + particle_X - box[1, 0])
+            f_bottom = max(0, radius - particle_Y)
+            f_up = max(0, radius + particle_Y - box[1, 1])
+            
+            forces[0, particle] += spring * (f_left - f_right)
+            forces[1, particle] += spring * (f_bottom - f_up)
+    
+
+# Checks for collisions with particles
+@njit(fastmath = True, parallel = True) #--Numba Ver--#
+def force_particle(N, radius,spring, width, X, Y, grid_index, forces, grid):
+
+    # Condition used to check if two particles have collided
+    condition = 4 * radius * radius # = (2 * radius) ** 2
+    
+    for particle in prange(N): #--Numba Ver--#
+    #for particle in range(N): #--Python Ver--#
+        # Index of particle in grid_index
+        particle_index_X, particle_index_Y = grid_index[0, particle], grid_index[1, particle]
+        
+        # Force to be added for particle
+        particle_force_X, particle_force_Y = 0, 0
+        
+        # Checks boxes around particle's box
+        for i in (-1, 0, 1):
+            for j in (-1, 0, 1):
+                neighbour_index = (particle_index_X + i) + (particle_index_Y + j) * width
+                if neighbour_index in grid:
+                    # Calculates forces between current particle and all other particles in current_box
+                    for neighbour in grid[neighbour_index]:
+                        # Positions of particle and neighbour
+                        particle_X, particle_Y = X[particle], Y[particle] 
+                        neighbour_X, neighbour_Y = X[neighbour], Y[neighbour]
+                            
+                        # Distance between the two particles (squared)
+                        diff_X = particle_X - neighbour_X
+                        diff_Y = particle_Y - neighbour_Y
+                        distance = diff_X * diff_X + diff_Y * diff_Y
+            
+                            # Checks if particles have collided
+                        if 0 < distance < condition:
+                            # Actual distance
+                            distance = math.sqrt(distance)
+                
+                            # Angle between particles
+                            alpha = math.atan2(diff_Y, diff_X)
+
+                            # Forces
+                            coeff = spring * (2 * radius - distance)
+
+                            force_X, force_Y = coeff * math.cos(alpha), coeff * math.sin(alpha)
+                
+                            particle_force_X += force_X
+                            particle_force_Y += force_Y
+
+        forces[0, particle] += particle_force_X
+        forces[1, particle] += particle_force_Y
+
+# Returns single time step
+def SimulationStep(x,v,dt,part,box,g):
+    #Assings radius and spring constant
+    radius = part["radius"]
+    spring = part["spring"]
+
+    #Assign N
+    N = len(x[0])
+
+    # Size of each box in grid
+    box_length = 2 * radius
+
+    # Used for grid
+    length = int(math.ceil((box[1][0] - box[0][0]) / box_length))
+    width = int(math.ceil((box[1][1] - box[0][1]) / box_length))
+
+    # Key = index of grid (int), Value = indexes of particles (list)
+    grid_empty = Dict.empty(key_type = types.int64, value_type = types.ListType(types.int64)) #--Numba Ver--#
+    #grid_empty = {} #--Python Ver--#
+    
+    # x and y coordinates of all particles
+    X, Y = x[0], x[1]
+
+    # Splits box into grid and assigns each particle to grid
+    grid_index = create_grid_index(N, box_length, length, width, X, Y)
+    grid = create_grid(N, width, grid_index, grid_empty)
+
+    # Forces of all particles
+    forces = np.zeros((2, N))
+
+    force_wall(N,radius,spring, X, Y, box, forces)
+    force_particle(N, radius,spring, width, X, Y, grid_index, forces, grid)
+    
+    x_new = x + dt * v + dt * dt * forces # Updated positions
+    v_new = (x_new - x) / dt # Updated velocities
+    
+    return x_new, v_new
