50af5014c3339e5701e463309358c0ffce02f64a
George Taylor <121175619+george-taylor03@users.noreply.github.com>
2026-03-09 11:35:42 +0000

    Updated Compression.
    Now calculates temperutate and pressure when compressed and displays results as a graph.

--- runCompression.py
+++ runCompression.py
@@ -10,7 +10,7 @@ from matplotlib.patches import Rectangle
 
 
 #Number of Particles
-p=6
+p=7
 N = 4**p
 
 #Particle Constant Radius and Elasticity
@@ -49,7 +49,7 @@ vini = 2.5
 # v
 v=2*(rand(2,N)-0.5)*vini
 
-#Set up graph
+# #Set up graph
 fig, ax = plt.subplots()
 scatter  = ax.scatter(x[0],x[1],s=2) 
 
@@ -66,23 +66,47 @@ plt.xlim(low[0] - diff[0], upp[0] + diff[0])
 plt.ylim(low[1] - diff[1], upp[1] + diff[1])
 
 #Wall speed
-a=100
+a=40
 
 #Settled Time tor
-tor1 = 10
+tor1 = 5
 tor2 = tor1 + (5 * np.sqrt(N))/a
+
+
+#Two time periods after settled from compression
+tor3 = tor2 + 20
+tor4 = tor3 + 20
 
 torTimes = np.arange(tor1,tor2,h)
 
-print(tor1/h)
-print(tor2/h)
+#Recorded times of temperature
+tempTimes = np.arange(tor3,tor4,h)
 
+#Recorded times of pressure
+pressTimes = []
+
+#Array of temperatures during tor3 and tor4
+T = []
+#
+
+#Array of pressure per wall initially before compression
+pInit = np.zeros(4)
+
+#Array of pressure per wall when compressed
+pComp = np.zeros(4)
+
+#Box Pressure values
+pBox=[]
+
+#Vary different wall speeds
 for i in range(int(loops)):
-    # print(i)
     #Change box dimensions
-    if i >= int(tor1/h) and i<=int(tor2/h):
-        upp[1] = 10 * np.sqrt(N) - a*(torTimes[i%loops] - tor1)
-        # low[1] =  a*(torTimes[i] - tor1)
+    if i >= int(tor1/h) and i<int(tor2/h):
+
+        #Locates index of tor times. Mods i so it starts from 0 and divides tor1 so it is inline with loops
+        loc = i-int(tor1/h)
+
+        upp[1] = 10 * np.sqrt(N) - a*(torTimes[loc] - tor1)
 
 
 
@@ -90,7 +114,39 @@ for i in range(int(loops)):
     box = np.vstack([low,upp])
 
     #Run Simulation
-    x , v = sim.SimulationStep(x, v, h, part, box, g)
+    x , v, forceW = sim.SimulationStep(x, v, h, part, box, g)
+
+        #Records initial pressure
+    if i<int(tor1/h):
+        #Pressure at each wall
+        pInit[0] += forceW[0] / box[1,1]
+        pInit[1] += forceW[1] / box[1,1]
+        pInit[2] += forceW[2] / box[1,0]
+        pInit[3] += forceW[3] / box[1,0]
+
+    #Measure Temperature and pressure after at tor3 and tor4
+    elif i >= int(tor3/h) and i<int(tor4/h):
+        T.append(np.average((v[0]**2 + v[1]**2)/2))
+
+        # #Pressure at each wall
+        pComp[0] += forceW[0] / box[1,1]
+        pComp[1] += forceW[1] / box[1,1]
+        pComp[2] += forceW[2] / box[1,0]
+        pComp[3] += forceW[3] / box[1,0]
+
+        #Finds pressure over serverl timesteps
+        if i % 20 == 0:
+            #Average perssure over serveral timesteps
+            pComp = pComp / 20
+            #Total box pressure
+            pBox.append(np.average(pComp))
+
+            #Reset Pressure
+            pComp = np.zeros(4)
+
+            #Add recorded pressure time. Muktiple by h to get to normal time not per timestep
+            pressTimes.append(i*h)
+
 
     if i%1==0:
         boxline.set_ydata([upp[1],upp[1]])
@@ -99,4 +155,23 @@ for i in range(int(loops)):
         data = np.c_[np.array(x[0]),np.array(x[1])]
         scatter.set_offsets(data)
         plt.pause(0.00001)
+
+#Initial Pressure. To get average must divide the number of timesteps it was taken over
+print(f"Inital Average Pressure {np.average(pInit / (tor1/h))}")
+
+#Compressed average Pressure
+print(f"Compressed Average Pressure {np.average(pBox)}")
+
+#Plot temperature of box between tor3 and tor 4
+plt.xlabel("Time (seconds)")
+plt.ylabel("Temperature (Joules)")
+plt.plot(tempTimes,T,label = f"Temperature over Time for Wall Speed a = {a}")
+
+plt.show()
+
+#Plot plot average pressure of all wals between tor3 and tor4
+plt.xlabel("Time Seconds")
+plt.ylabel("Pressure")
+plt.plot(pressTimes,pBox, label = f"Pressure over Time for Wall Speed a = {a}")
+
 plt.show()
