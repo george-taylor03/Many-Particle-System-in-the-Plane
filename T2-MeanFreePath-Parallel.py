e4407a7abd90d7e99c6544b819dbaf978464afbd
ZorawarSidhu <zorawarsidhu@hotmail.com>
2026-03-14 14:52:17 +0000

    Changed particle collision counting logic

--- T2-MeanFreePath-Parallel.py
+++ T2-MeanFreePath-Parallel.py
@@ -256,10 +256,9 @@ for i in range(int(end_time / dt)):
 
     # Particle collisions
     if new_particle_collision_particles:
-        for particle, neighbour in new_particle_collision_particles:
-            if (particle, neighbour) not in old_particle_collision_particles:
-                particle_collision_count[particle] += 1
-                particle_collision_count[neighbour] += 1
+        for particle, neighbour in new_particle_collision_particles - old_particle_collision_particles:
+            particle_collision_count[particle] += 1
+            particle_collision_count[neighbour] += 1
 
         old_particle_collision_particles = new_particle_collision_particles
     
