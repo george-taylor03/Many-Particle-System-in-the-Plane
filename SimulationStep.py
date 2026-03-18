5ec9e2ca512caf62d1482ccb2d1361e2e4b03dee
ZorawarSidhu <zorawarsidhu@hotmail.com>
2026-03-18 15:49:08 +0000

    Changed particle_dicts back to particle_lists + removed some comments

--- SimulationStep.py
+++ SimulationStep.py
@@ -3,44 +3,19 @@ import matplotlib.pyplot as plt
 import math
 import time
 from matplotlib.patches import Rectangle
-from numba import njit, prange, int64, get_num_threads, get_thread_id #--Numba Ver--#
-from numba.typed import List, Dict #--Numba Ver--#
-from numba.core import types #--Numba Ver--#
-
-#~---------------------------------------------------------------------------------------------------------------------------------------------------~#
-
-# Numba Notes:
-    # User manual - https://numba.readthedocs.io/en/stable/user/index.html
-
-    # Currently only works for Numba version, ignore Python lines for now
+from numba import njit, prange, int64, get_num_threads, get_thread_id
+from numba.typed import List, Dict
+from numba.core import types
     
-    # Lines with comments '#--Numba Ver--#' -> uncommenting = Numba version
-    # Lines with comments '#--Python Ver--#' -> uncommenting = Python version
-        # Choose one version to run
-    # If creating code in Python first -> avoid dictionaries where possible (initializing a numba.typed.dict (like in simulation_step) can be slow)
-
-# Potential Improvements:
-    # create_grid_index and create_grid can be merged -> might make a small change, better readability when separate (in my opinion)
-    # Find way to reduce math.function (e.g. math.cos) in force_particle, ideally get rid of all -> sqrt not as expensive as others
-    # Go through Performance Improvements (Link, User Manual -> Performance Tips) on njit functions, see if time goes down
-        # Already done: fastmath, parallel
-
-# Small optimisation notes:
-    # x * x faster than x ** 2
-    # math.function faster for numbers, np.function faster for arrays
-    # array[:, n] slower than array[0, n], array[1, n], ...
-    # Splitting x into X (=x[0]) and Y (=x[1]) usually faster
-
-#~---------------------------------------------------------------------------------------------------------------------------------------------------~#
+# Numba user manual: - https://numba.readthedocs.io/en/stable/user/index.html
 
 np.random.seed(1)
 ran = np.random.rand
 
 # Creates list of all particle's indexes
-@njit #--Numba Ver--#
+@njit
 def create_grid_index(N, box_length, length, width, X, Y):
-    grid_index = np.empty((2, N), dtype = int64) #--Numba Ver--#
-    #grid_index = np.empty((2, N)) #--Python Ver--#
+    grid_index = np.empty((2, N), dtype = int64)
     
     for particle in range(N):
         # Position of particle
@@ -55,26 +30,24 @@ def create_grid_index(N, box_length, length, width, X, Y):
     return grid_index
 
 # Returns grid of all particles
-@njit #--Numba Ver--#
+@njit
 def create_grid(N, length, grid_index, grid):
     for particle in range(N):
         # Particle's index
         particle_col, particle_row = grid_index[0, particle], grid_index[1, particle]
 
         # Converts (row, col) into integer - https://stackoverflow.com/questions/1730961/convert-a-2d-array-index-into-a-1d-index
-        key = int64(particle_row * length + particle_col) #--Numba Ver--#
-        #key = particle_row * length + particle_col #--Python Ver--#
+        key = int64(particle_row * length + particle_col)
         
         if key not in grid:
-            grid[key] = List.empty_list(int64) # --Numba Ver--
-            #grid[key] = [] #--Python Ver--#
+            grid[key] = List.empty_list(int64)
         
         grid[key].append(particle)
         
     return grid
 
 # Checks for collisions with walls
-@njit(fastmath = True, parallel = True) #--Numba Ver--# 
+@njit(fastmath = True, parallel = True)
 def force_wall(N, radius, spring, X, Y, box, forces):
 
     #Force on verticle walls
@@ -87,8 +60,7 @@ def force_wall(N, radius, spring, X, Y, box, forces):
     wall_collision_particles = np.zeros(N, dtype = np.bool_)
 
 
-    for particle in prange(N): #--Numba Ver--#
-    #for particle in range(N): #--Python Ver--#
+    for particle in prange(N):
         # Position of particle
         particle_X, particle_Y = X[particle], Y[particle]
 
@@ -120,13 +92,12 @@ def force_wall(N, radius, spring, X, Y, box, forces):
     return forces_walls, forces, wall_collision_particles, vWalls
 
 # Checks for collisions with particles
-@njit(fastmath = True, parallel = True) #--Numba Ver--#
-def force_particle(N, radius, spring, width, X, Y, grid_index, forces, grid, particle_dicts):
+@njit(fastmath = True, parallel = True)
+def force_particle(N, radius, spring, width, X, Y, grid_index, forces, grid, particle_collision_lists):
     # Condition used to check if two particles have collided
     condition = 4 * radius * radius # = (2 * radius) ** 2
     
-    for particle in prange(N): #--Numba Ver--#
-    #for particle in range(N): #--Python Ver--#
+    for particle in prange(N):
         # Index of particle in grid_index
         particle_index_X, particle_index_Y = grid_index[0, particle], grid_index[1, particle]
         
@@ -167,9 +138,9 @@ def force_particle(N, radius, spring, width, X, Y, grid_index, forces, grid, par
                             particle_force_X += force_X
                             particle_force_Y += force_Y
 
-                            # Adding (particle, neighbour) to thread's dictionary
+                            # Adding (particle, neighbour) to thread's list
                             if particle < neighbour:
-                                particle_dicts[get_thread_id()][(int64(particle), neighbour)] = True
+                                particle_collision_lists[get_thread_id()].append((int64(particle), neighbour))
 
         forces[0, particle] += particle_force_X
         forces[1, particle] += particle_force_Y
@@ -196,15 +167,12 @@ def SimulationStep(x, v, dt, part, box, g):
     width = int(math.ceil((box[1, 1] - box[0, 1]) / box_length))
 
     # Key = index of grid (int), Value = indexes of particles (list)
-    grid_empty = Dict.empty(key_type = types.int64, value_type = types.ListType(types.int64)) #--Numba Ver--#
-    #grid_empty = {} #--Python Ver--#
+    grid_empty = Dict.empty(key_type = types.int64, value_type = types.ListType(types.int64))
     
-    # List of dictionaries for particle forces, each thread has it's own dictionary
-    particle_dicts = []
-    
+    # List of lists for particle collisions, each thread has it's own list (to avoid race condition in force_particle)
+    particle_collision_lists = []
     for i in range(get_num_threads()):
-        # value doesn't matter for dictionaries, only need keys (= (particle index, neighbour index))
-        particle_dicts.append(Dict.empty(key_type = types.UniTuple(types.int64, 2), value_type = types.boolean))
+        particle_collision_lists.append(List.empty_list(types.UniTuple(types.int64, 2)))
 
     # Splits box into grid and assigns each particle to grid
     grid_index = create_grid_index(N, box_length, length, width, X, Y)
@@ -214,16 +182,13 @@ def SimulationStep(x, v, dt, part, box, g):
     forces = np.zeros((2, N))
 
     # Wall forces
-    forces_walls, forces, wall_collision_particles, vWalls = force_wall(N,radius,spring, X, Y, box, forces)
+    forces_walls, forces, wall_collision_particles, vWalls = force_wall(N, radius, spring, X, Y, box, forces)
 
     # Particle forces
-    forces = force_particle(N, radius,spring, width, X, Y, grid_index, forces, grid,particle_dicts)
+    forces = force_particle(N, radius, spring, width, X, Y, grid_index, forces, grid, particle_collision_lists)
     
-    particle_collision_particles = set()
-    
-    for dict in particle_dicts:
-        for key in dict:
-            particle_collision_particles.add(key)
+    # Adds all elements in each list in particle_collision_lists to a set
+    particle_collision_particles = set().union(*particle_collision_lists)
 
     # Gravity forces
     forces[1, :] += -g
