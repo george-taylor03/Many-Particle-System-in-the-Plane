0940f3f3c9efcff37b5cb1e4c1de13b2b4e77879
ZorawarSidhu <zorawarsidhu@hotmail.com>
2026-03-24 19:02:49 +0000

    Final version
    Main differences: Added Fa = -Fb, Removed trig

--- SimulationStep.py
+++ SimulationStep.py
@@ -8,10 +8,13 @@ from numba.core import types
 
 set_num_threads(4)
 
+# Tuple type for cell's index and indexes of 2 colliding particles
+tuple_type = types.UniTuple(int64, 2)
+
 # Creates grid with all particle's indexes
-# Returns grid (dict) + cell index of particles (array)
+# Returns: grid, cell index of particles
 @njit
-def create_grid(N, cell_length, length, X, Y, grid):
+def create_grid(N, cell_length, X, Y, grid):
     cell_index = np.zeros((2, N), dtype = np.int64)
     
     cell_length_inv = 1 / cell_length
@@ -21,14 +24,13 @@ def create_grid(N, cell_length, length, X, Y, grid):
         particle_X, particle_Y = X[particle], Y[particle]
         
         # Particle's cell index
-        particle_index_X, particle_index_Y = int(particle_X * cell_length_inv), int(particle_Y * cell_length_inv)
+        particle_index_X, particle_index_Y = math.floor(particle_X * cell_length_inv), math.floor(particle_Y * cell_length_inv)
 
         cell_index[0, particle] = particle_index_X
         cell_index[1, particle] = particle_index_Y
-        
-        # Converts cell index into integer - https://stackoverflow.com/questions/1730961/convert-a-2d-array-index-into-a-1d-index
-        key = particle_index_Y * length + particle_index_X
 
+        key = (particle_index_X, particle_index_Y)
+        
         if key not in grid:
             grid[key] = List.empty_list(int64)
         
@@ -37,11 +39,11 @@ def create_grid(N, cell_length, length, X, Y, grid):
     return grid, cell_index
 
 # Calculates forces for all particles
-# Returns forces (array) + forces on vertical walls (array) + total forces for each wall (array) + particles currently colliding with a wall (array)
+# Returns: forces, forces on vertical walls, total forces for each wall, particles currently colliding with a wall
 @njit(fastmath = True, parallel = True)
-def calculate_forces(N, radius, spring, g, cell_length, length, X, Y, cell_index, box, particle_collision_lists, grid):
+def calculate_forces(N, radius, spring, g, X, Y, cell_index, box, particle_collision_lists, grid):
     # Forces for all particles
-    forces = np.zeros((2, N), dtype = np.float64)
+    forces = np.zeros((get_num_threads(), 2, N), dtype = np.float64)
     
     # Force on verticle walls
     v_walls = np.zeros(N, dtype = np.float64)
@@ -56,6 +58,8 @@ def calculate_forces(N, radius, spring, g, cell_length, length, X, Y, cell_index
     collision_condition = 4 * radius * radius # = (2 * radius) ** 2
 
     for particle in prange(N):
+        thread_id = get_thread_id()
+        
         # Position of particle
         particle_X, particle_Y = X[particle], Y[particle]
 
@@ -79,8 +83,8 @@ def calculate_forces(N, radius, spring, g, cell_length, length, X, Y, cell_index
             forces_upper_wall += f_upper
 
             # Wall force for particle
-            forces[0, particle] += spring * (f_left - f_right)
-            forces[1, particle] += spring * (f_bottom - f_upper)
+            forces[thread_id, 0, particle] += spring * (f_left - f_right)
+            forces[thread_id, 1, particle] += spring * (f_bottom - f_upper)
 
             # Adds verticle wall forces
             v_walls[particle] += f_left - f_right
@@ -96,11 +100,12 @@ def calculate_forces(N, radius, spring, g, cell_length, length, X, Y, cell_index
         # Checks cells around particle's cell
         for i in (-1, 0, 1):
             for j in (-1, 0, 1):
-                neighbour_index = (particle_index_Y + j) * length + (particle_index_X + i) # Note: check and update
+                neighbour_index = (particle_index_X + i, particle_index_Y + j)
                 if neighbour_index in grid:
                     # Calculates forces between current particle and all other particles in current cell
                     for neighbour in grid[neighbour_index]:
-                        if neighbour != particle:
+                        # Using Fa = -Fb
+                        if particle < neighbour:
                             # Position of neighbour
                             neighbour_X, neighbour_Y = X[neighbour], Y[neighbour]
                             
@@ -113,51 +118,53 @@ def calculate_forces(N, radius, spring, g, cell_length, length, X, Y, cell_index
                             if 0 < distance < collision_condition:
                                 # Actual distance
                                 distance = math.sqrt(distance)
-                
-                                # Angle between particles
-                                alpha = math.atan2(diff_Y, diff_X)
 
                                 # Forces
-                                coeff = spring * (2 * radius - distance)
-
-                                force_X, force_Y = coeff * math.cos(alpha), coeff * math.sin(alpha)
+                                # Uses cos(alpha) = x / r and sin (alpha) = y / r
+                                coeff = spring * (2 * radius - distance) / distance
+                                
+                                force_X, force_Y = coeff * diff_X, coeff * diff_Y
                 
                                 particle_force_X += force_X
                                 particle_force_Y += force_Y
 
+                                # Adds -Fa to neighbour in array
+                                forces[thread_id, 0, neighbour] -= force_X
+                                forces[thread_id, 1, neighbour] -= force_Y
+
                                 # Adding (particle, neighbour) to thread's list
-                                if particle < neighbour:
-                                    particle_collision_lists[get_thread_id()].append((int64(particle), neighbour))
+                                particle_collision_lists[thread_id].append((int64(particle), neighbour))
 
         # Adds total force of particle to array
-        forces[0, particle] += particle_force_X
-        forces[1, particle] += particle_force_Y
+        forces[thread_id, 0, particle] += particle_force_X
+        forces[thread_id, 1, particle] += particle_force_Y
 
     
         # Gravity forces
-        forces[1, particle] -= g
+        forces[thread_id, 1, particle] -= g
 
+
+    # Joining seperate threads into a (2, N) array
+    forces = np.sum(forces, axis = 0)
     
     forces_walls = np.array([forces_left_wall, forces_right_wall, forces_bottom_wall, forces_upper_wall])
     
     return forces, v_walls, forces_walls, wall_collision_particles
 
-# Returns single time step
-def SimulationStep(x:np.ndarray, v:np.ndarray, dt:float, part:dict, box:np.ndarray, g:float) -> tuple[np.ndarray,np.ndarray]:
-    """
-    Preforms one single step forward in time
+# Creates list of empty lists for tracking particle collisions
+# Returns: list of empty lists
+@njit
+def create_particle_collision_lists():
+    particle_collision_lists = []
+    for thread in range(get_num_threads()):
+        particle_collision_lists.append(List.empty_list(tuple_type))
 
-    Args:
-        dt (float): Time step size
-        x (2xN array): Current particle positions (first row are horizontal coordinates, second row are vertical coordinates)
-        v (2xN array): Current particle velocities (first row are horizontal coordinates, second row are vertical coordinates)
-        part (dictionary['radius','spring']): Stores properties of the particle (radius r of all particles and spring constant of all particles)
-        box (2x2 array): Array storing the corners of the box (np.vstack([low,upp]))
-        g (float): Single number, the amount g of gravity
+    return particle_collision_lists
 
-    Returns:
-        tuple: xnew(2xN array), vnew(2xN array) which are the updated position and velocity
-    """
+# Simulates single time step
+# Returns: new positions, new velocities, total forces for each wall, distance each particle travelled,
+#          particles colliding with a wall, particles colliding with other particles, forces on vertical walls
+def SimulationStep(x, v, dt, part, box, g):
     # N
     N = len(x[0])
     
@@ -171,22 +178,17 @@ def SimulationStep(x:np.ndarray, v:np.ndarray, dt:float, part:dict, box:np.ndarr
     # Size of each cell in grid
     cell_length = 2 * radius
 
-    # Number of cells along grid
-    length = int(math.ceil((box[1, 1] - box[0, 1]) / cell_length))
-
-    # Key = index of grid (int), Value = indexes of particles (list)
-    grid_empty = Dict.empty(key_type = types.int64, value_type = types.ListType(types.int64))
+    # Key = index of cell (list), Value = indexes of particles (list)
+    grid_empty = Dict.empty(key_type = tuple_type, value_type = types.ListType(int64))
     
-    # List of lists for particle collisions, each thread has it's own list (to avoid race condition in force_particle)
-    particle_collision_lists = []
-    for i in range(get_num_threads()):
-        particle_collision_lists.append(List.empty_list(types.UniTuple(types.int64, 2)))
+    # List of lists for particle collisions, each thread has it's own list (to avoid race condition in calculate_forces)
+    particle_collision_lists = create_particle_collision_lists()
 
     # Fills grid and assigns each particle to cell
-    grid, cell_index = create_grid(N, cell_length, length, X, Y, grid_empty)
+    grid, cell_index = create_grid(N, cell_length, X, Y, grid_empty)
 
     # Forces of all particles
-    forces, v_walls, forces_walls, wall_collision_particles = calculate_forces(N, radius, spring, g, cell_length, length, X, Y, cell_index, box, particle_collision_lists, grid)
+    forces, v_walls, forces_walls, wall_collision_particles = calculate_forces(N, radius, spring, g, X, Y, cell_index, box, particle_collision_lists, grid)
     
     # Adds all elements in each list in particle_collision_lists to a set
     particle_collision_particles = set().union(*particle_collision_lists)
